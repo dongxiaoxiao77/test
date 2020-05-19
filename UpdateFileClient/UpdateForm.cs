8c3a5be6a00a1d4e7fee37c68c0f89c01129b2ed
dongxiaoxiao <dongxiaoxiao@benlai.com>
2020-05-19 10:49:48 +0800

    test

---
+++ UpdateFileClient/UpdateForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+using UpdateFileCommon;
+
+
+namespace UpdateFileCommon
+{
+    public partial class UpdateForm : Form
+    {
+        WebClient client;
+        int fileIndex = 0;
+        int fileListCount = 0;
+        string ServerPath = null;
+        string UpdateProgramDic = null;
+        string UpdateProgramName = null;
+        List<string> fileList = new List<string>();
+
+        public UpdateForm()
+        {
+            InitializeComponent();
+
+            this.Load += delegate
+            {
+                lblState.Text = "正在连接服务器...";
+                lblRemark.Visible = false;
+
+                if (VersionHelper.IsRequiredUpdate())
+                {
+                    Thread thread = new Thread(DownLoadFileMethod);
+                    thread.IsBackground = true;
+                    thread.Start();
+                }
+                else
+                {
+                    MessageBox.Show("已是最新版本", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Application.Exit();
+                }
+            };
+
+            this.FormClosing += delegate
+            {
+                if (client != null)
+                    client.CancelAsync();
+            };
+        }
+
+        /// <summary>
+        /// 开始下载文件
+        /// </summary>
+        public void DownLoadFileMethod()
+        {
+            string xmlPath = System.AppDomain.CurrentDomain.BaseDirectory + "UpdateFile.xml";
+            fileList = VersionHelper.GetNeedUpdateFile();
+            XmlDocument localDoc = new XmlDocument();
+            localDoc.Load(xmlPath);
+            var serverurl = VersionHelper.GetLoaclServerConfigURL(localDoc);
+            ServerPath = XMLHelper.GetXmlNodeValueByXpath(serverurl, "/Config/ServerURL");
+            UpdateProgramDic = XMLHelper.GetXmlNodeValueByXpath(localDoc, "/Config/UpdateProgramDirectory");
+            UpdateProgramName = XMLHelper.GetXmlNodeValueByXpath(localDoc, "/Config/ClientProgramName");
+            fileListCount = fileList.Count;
+            this.BeginInvoke((ThreadStart)delegate
+            {
+                lblState.Text = "正在下载文件：";
+                lblRemark.Visible = true;
+                DownloadFile();
+            });
+        }
+
+        public void DownloadFile()
+        {
+            //下载文件
+            if (fileIndex < fileListCount)
+            {
+                try
+                {
+                    client = new WebClient();
+                    lblRemark.Text = string.Format("正在下载第 {0} 个文件（共 {1} 个）", fileIndex + 1, fileListCount);
+                    var uri = new Uri(ServerPath + fileList[fileIndex]);
+                    var savePath = System.AppDomain.CurrentDomain.BaseDirectory;
+                    if (fileList[fileIndex] == UpdateProgramName)
+                        savePath += UpdateProgramDic + "\\";
+                    else
+                        savePath = System.AppDomain.CurrentDomain.BaseDirectory;
+                    if (!Directory.Exists(savePath))//如果不存在就创建file文件夹
+                    {
+                        Directory.CreateDirectory(savePath);
+                    }
+                    //判断文件是否存在
+                    if (UrlFileExists(uri.ToString()))
+                    {
+                        //开始下载文件
+                        client.DownloadProgressChanged += OnDownloadProgressChanged;
+                        client.DownloadFileAsync(uri, savePath + fileList[fileIndex].Replace("/", "\\"));
+                        client.DownloadFileCompleted += OnDownloadComplete;
+                    }
+                    else
+                    {
+                        MessageBox.Show(string.Format("下载更新出错：{0}文件不存在", uri.ToString()), "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("下载更新出错：" + ex.Message, "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            //下载完成
+            else
+            {
+                string xmlPath = System.AppDomain.CurrentDomain.BaseDirectory + "UpdateFile.xml";
+                XmlDocument clientDoc = new XmlDocument();
+                clientDoc.Load(xmlPath); //加载XML文档
+                var serverurl = VersionHelper.GetLoaclServerConfigURL(clientDoc);
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(serverurl); //加载XML文档
+                //获取服务器版本号
+                var vernode = XMLHelper.GetXmlNodeByXpath(xmlDoc, "/Config/Version/Item");
+                var ver = XMLHelper.GetNodeAttributeValue(vernode, "Version");
+                var vervalue = XMLHelper.GetNodeAttributeValue(vernode, "VersionValue");
+                //更新本地版本号
+                XMLHelper.CreateOrUpdateXmlNodeByXPath(clientDoc, "/Config", "LocalVersion", ver);
+                XMLHelper.CreateOrUpdateXmlNodeByXPath(clientDoc, "/Config", "LocalVersionValue", vervalue);
+                //更新文件列表
+                var serverNodes = XMLHelper.GetXmlNodeListByXpath(xmlDoc, "/Config/File/Item");
+                var xmlText = "";
+                foreach (XmlNode item in serverNodes)
+                {
+                    var name = XMLHelper.GetNodeAttributeValue(item, "Name");
+                    var version = XMLHelper.GetNodeAttributeValue(item, "Version");
+                    var date = XMLHelper.GetNodeAttributeValue(item, "Date");
+                    xmlText += String.Format("<Item Name=\"{0}\" Version=\"{1}\" Date=\"{2}\"></Item>", name, version, date);
+                }
+                XMLHelper.CreateOrUpdateXmlNodeByXPath(clientDoc, "/Config", "File", xmlText);
+                clientDoc.Save(xmlPath);
+                MessageBox.Show("下载更新完成，请重新启动程序", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// 文件是否存在
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private bool UrlFileExists(string uri)
+        {
+            HttpWebRequest req = null;
+            HttpWebResponse res = null;
+            try
+            {
+                req = (HttpWebRequest)WebRequest.Create(uri);
+                req.Method = "HEAD";
+                req.Timeout = 100;
+                res = (HttpWebResponse)req.GetResponse();
+                return (res.StatusCode == HttpStatusCode.OK);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (res != null)
+                {
+                    res.Close();
+                    res = null;
+                }
+                if (req != null)
+                {
+                    req.Abort();
+                    req = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 进度改变
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// 下载完成
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnDownloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            fileIndex += 1;
+            DownloadFile();
+        }
+    }
+}
---
+++ UpdateFileCommon/PromptingForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace UpdateFileCommon
+{
+    partial class PromptingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PromptingForm));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnNextShow = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnNextShow);
+            this.flowLayoutPanel1.Controls.Add(this.btnUpdate);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 259);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(439, 42);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnNextShow
+            // 
+            this.btnNextShow.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNextShow.Image = global::UpdateFileCommon.Properties.Resources.hand_point;
+            this.btnNextShow.Location = new System.Drawing.Point(344, 3);
+            this.btnNextShow.Name = "btnNextShow";
+            this.btnNextShow.Size = new System.Drawing.Size(92, 32);
+            this.btnNextShow.TabIndex = 0;
+            this.btnNextShow.Text = "下次提醒";
+            this.btnNextShow.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNextShow.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNextShow.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnUpdate.Image = global::UpdateFileCommon.Properties.Resources.download;
+            this.btnUpdate.Location = new System.Drawing.Point(263, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 33);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "更新";
+            this.btnUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnUpdate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtDescription.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtDescription.Location = new System.Drawing.Point(0, 0);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(439, 259);
+            this.txtDescription.TabIndex = 1;
+            // 
+            // PromptingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(235)))), ((int)(((byte)(247)))));
+            this.ClientSize = new System.Drawing.Size(439, 301);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PromptingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "更新提醒";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnNextShow;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
